a565224486e6e683993882495391fcb5d177e858
John Brandenburg <jcb7d7@umsl.edu>
2016-10-22 18:20:47 -0500

    dashboard template

--- findHomeWeb/routes/index.js
+++ findHomeWeb/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Humanitarian Resources' });
+});
+
+router.post('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard' });
 });
 
 module.exports = router;
